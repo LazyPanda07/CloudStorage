2c5ff15a338db0f46952c38e888463dc827787bc
Semen Gricenko <semengricenko@gmail.com>
2020-07-13 01:33:52 +0500

    Added some data base functions. uploadFunction insert into data base information about new file

--- DataBaseServer/src/CloudDataBase.cpp
+++ DataBaseServer/src/CloudDataBase.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 namespace db
 {
-	int CloudDataBase::getId(const string& login, unsigned int password) const
+	int CloudDataBase::getId(const string& login) const
 	{
 		int id = -1;
-		const string condition = "login = '" + login + "' AND password = " + to_string(password);
+		const string condition = "login = '" + login + "'";
 
 		auto callback = [](void* userData, int argc, char** argv, char** columns)
 		{
@@ -30,6 +30,80 @@ namespace db
 		);
 
 		return id;
+	}
+
+	string CloudDataBase::getLogin(int id) const
+	{
+		string login;
+		const string condition = "id = " + to_string(id);
+
+		auto callback = [](void* userData, int argc, char** argv, char** columns)
+		{
+			(*static_cast<string*>(userData)) = argv[0];
+
+			return 0;
+		};
+
+		db.select
+		(
+			usersTable,
+			condition,
+			callback,
+			&login,
+			"login"
+		);
+
+		return login;
+	}
+
+	unsigned int CloudDataBase::getPassword(const string& login) const
+	{
+		unsigned int password;
+		const string condition = "login = '" + login + "'";
+
+		auto callback = [](void* userData, int argc, char** argv, char** columns)
+		{
+			char* end;
+			(*static_cast<unsigned int*>(userData)) = strtoul(argv[0], &end, 10);
+
+			return 0;
+		};
+
+		db.select
+		(
+			usersTable,
+			condition,
+			callback,
+			&password,
+			"password"
+		);
+
+		return password;
+	}
+
+	unsigned int CloudDataBase::getPassword(int id) const
+	{
+		unsigned int password;
+		const string condition = "id = " + to_string(id);
+
+		auto callback = [](void* userData, int argc, char** argv, char** columns)
+		{
+			char* end;
+			(*static_cast<unsigned int*>(userData)) = strtoul(argv[0], &end, 10);
+
+			return 0;
+		};
+
+		db.select
+		(
+			usersTable,
+			condition,
+			callback,
+			&password,
+			"password"
+		);
+
+		return password;
 	}
 
 	unsigned int CloudDataBase::customHash(const string& source)
@@ -56,6 +130,42 @@ namespace db
 			"login", "VARCHAR(32) UNIQUE NOT NULL",
 			"password", "INTEGER NOT NULL"
 		);
+
+		db.createTable
+		(
+			filesTable,
+			"id", "INTEGER PRIMARY KEY AUTOINCREMENT",
+			"userId", "INTEGER NOT NULL",
+			"fileName", "TEXT NOT NULL",
+			"filePath", "TEXT DEFAULT 'Home' NOT NULL",
+			"fileExtension", "TEXT DEFAULT '' NOT NULL",
+			"fileSize", "INTEGER DEFAULT 0 NOT NULL",
+			"uploadData", "TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL",
+			"dateOfChange", "TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL",
+			"FOREIGN KEY (userId)", "REFERENCES Users(id)"
+		);
+	}
+
+	bool CloudDataBase::uploadFile(const string& login, std::string&& fileName, std::string&& filePath, std::string&& fileExtension) const
+	{
+		try
+		{
+			db.insert
+			(
+				filesTable,
+				"userId", this->getId(login),
+				"fileName", move(fileName),
+				"filePath", move(filePath),
+				"fileExtension", move(fileExtension)
+			);
+
+			return true;
+		}
+		catch (const db::DataBaseException& e)
+		{
+			Log::warning(e.what());
+			return false;
+		}
 	}
 
 	string CloudDataBase::registration(const string& login, const string& password) const
@@ -80,6 +190,32 @@ namespace db
 
 	bool CloudDataBase::authorization(const string& login, const string& password) const
 	{
-		return this->getId(login, customHash(password)) != -1;
+		int id = -1;
+		const string condition = "login = '" + login + "' AND password = " + to_string(customHash(password));
+
+		auto callback = [](void* userData, int argc, char** argv, char** columns)
+		{
+			(*static_cast<int*>(userData)) = atoi(argv[0]);
+
+			return 0;
+		};
+
+		db.select
+		(
+			usersTable,
+			condition,
+			callback,
+			&id,
+			"id"
+		);
+
+		if (id != -1)
+		{
+			return id;
+		}
+
+		Log::warning("Fail authorization with login: {} and password: {}\n", login, password);
+
+		return false;
 	}
 }
--- DataBaseServer/src/CloudDataBase.h
+++ DataBaseServer/src/CloudDataBase.h
@@ -10,12 +10,20 @@ namespace db
 		DataBaseOperations db;
 
 	private:
-		int getId(const std::string& login, unsigned int password) const;
+		int getId(const std::string& login) const;
+
+		std::string getLogin(int id) const;
+
+		unsigned int getPassword(const std::string& login) const;
+
+		unsigned int getPassword(int id) const;
 
 		static unsigned int customHash(const std::string& source);
 
 	public:
 		CloudDataBase();
+
+		bool uploadFile(const std::string& login, std::string&& fileName, std::string&& filePath, std::string&& fileExtension) const;
 
 		std::string registration(const std::string& login, const std::string& password) const;
 
